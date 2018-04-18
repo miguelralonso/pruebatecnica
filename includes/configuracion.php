4c54b186297d717c2e69ef788bb111d7ae4ace3f
miguelralonso <38511634+miguelralonso@users.noreply.github.com>
2018-04-19 00:56:33 +0200

    Cambios claves

--- includes/configuracion.php
+++ includes/configuracion.php
@@ -2,16 +2,16 @@
 
 
 // Datos de conexión a la base de datos
-$conexion = new PDO('mysql:host=localhost;dbname=elogia', 'root', '');
+$conexion = new PDO('mysql:host=xxxxxxxxx;dbname=elogia', 'root', '');
 
 // Datos de conexión con la API de twitter
 $parametrosAPITwitter = array(
-    'oauth_access_token' => "301542741-faTyJlhjRfRV3X04VZB37uKWPTelUxuVTEhtjqdJ",
-    'oauth_access_token_secret' => "qn0z3OHqeosAfiMChddrO1HkKxu3IYpYEKTO7sbbercyZ",
-    'consumer_key' => "QNN6AWk7Wc5aQyEldJQ0p89BL",
-    'consumer_secret' => "s2vgCtakuKGymwpUFbKpJQP5cKG2jNO9Wz4sMD1g02NLp9cTAE"
+    'oauth_access_token' => "xxxxxxxxxxxxxxxx",
+    'oauth_access_token_secret' => "xxxxxxxxxxxxxxxx",
+    'consumer_key' => "xxxxxxxxxxxxxxxx",
+    'consumer_secret' => "xxxxxxxxxxxxxxxx"
 );
 
-$APITokenMonkeyLearn = '3177fdec521310b0171e100e5f3348d0d2a7a690';
+$APITokenMonkeyLearn = 'xxxxxxxxxxxxxxxx';
 
 ?>
